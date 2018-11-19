292305b20264815c13be8cc07cca7acf9dfa0e76
Chryssa <chryssa.stavr@gmail.com>
2018-11-19 18:38:06 +0200

    Bootstrap JQuery

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//alert("working")
+
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 500 } );
+    $("#carousel-pause").click(function(){
+        $("#mycarousel").carousel('pause');
+    });
+    $("#carousel-play").click(function(){
+        $("#mycarousel").carousel('cycle');
+    });
+});
